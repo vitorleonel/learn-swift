bd8c9910ee6ee46ee27f860c992081139a6197d6
Vitor Leonel <falecom@vitorleonel.com>
2020-09-20 11:02:49 -0300

    Initial Commit

---
+++ ewallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ewallet
+//
+//  Created by Vitor Leonel on 20/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
